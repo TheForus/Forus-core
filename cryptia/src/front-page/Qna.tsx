f6aae57660d8c3af2d22d39408ecc4225e5d2cb0
saadabban76 <115649011+saadabban76@users.noreply.github.com>
2023-05-09 12:27:50 +0530

    minor changes

--- cryptia/src/front-page/Qna.tsx
+++ cryptia/src/front-page/Qna.tsx
@@ -15,7 +15,7 @@ const Qna = (props: Props) => {
   return (
     <div
       id="faq"
-      className="p-5 py-10 flex flex-col items-center justify-center bg-[#10F1B4] dark:bg-bgBlack"
+      className="p-5 py-10 flex flex-col items-center justify-center bg-[#10F1B4] dark:bg-black"
     >
       <h1 className="montserrat-subheading text-2xl mb-6 text-white dark:text-[#435864] border-b pb-2 border-gray-200">
         FAQ
