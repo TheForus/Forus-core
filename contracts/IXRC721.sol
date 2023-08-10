6f5f6d3115e952b0ad6fded7c0013a9bed27433e
maaz <maazweb3@gmail.com>
2023-08-10 16:20:28 -0700

    initializing

--- contracts/IXRC721.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.16;
-
-interface IXRC721 {
-    function transferFrom(address from, address to, uint256 tokenId) external;
-    function ownerOf(uint256 tokenId) external view returns (address);
-}
--- contracts/Logs.sol
+++ contracts/Logs.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-<<<<<<< HEAD
+
+
 import "./IERC20.sol";
 import "./IERC721.sol";
-=======
-import "./IXRC20.sol";
-import "./IXRC721.sol";
->>>>>>> origin/master
+
 
 
 /**
  * @title Logs
 <<<<<<< HEAD
+<<<<<<< HEAD
  * @dev A smart contract that enables the publication of public keys on the blockchain and facilitates fund transfers to a specified address.
+=======
+ * @dev A smart contract that enables the publication of pub keys on the blockchain and facilitates fund transfers to a specified address.
+>>>>>>> origin/master
 =======
  * @dev A smart contract that enables the publication of pub keys on the blockchain and facilitates fund transfers to a specified address.
 >>>>>>> origin/master
@@ -22,15 +24,21 @@ import "./IXRC721.sol";
  * These keys serve as authorization for the associated transactions and provide an additional layer of security.
  *
 <<<<<<< HEAD
+<<<<<<< HEAD
  * Users publish their public keys by invoking the appropriate functions in the contract.
  * The contract maintains a log of published keys and keeps track of the total funds received.
  *
  * Users can transfer ERC, ERC20 tokens, and ERC721 tokens to a designated recipient address, authorized by their published keys.
 =======
+=======
+>>>>>>> origin/master
  * Users can publish their pub keys by invoking the appropriate functions in the contract.
  * The contract maintains a log of published keys and keeps track of the total funds received.
  *
  * Users can transfer XDC, XRC20 tokens, and XRC721 tokens to a designated recipient address, authorized by their published keys.
+<<<<<<< HEAD
+>>>>>>> origin/master
+=======
 >>>>>>> origin/master
  */
 
@@ -75,11 +83,8 @@ contract Logs {
         logs.push(publickeys(r, s, v));
     }
 
-<<<<<<< HEAD
+
     function Transfer(
-=======
-    function TransferXDC(
->>>>>>> origin/master
         bytes32 r,
         bytes32 s,
         bytes4 secret,
@@ -102,11 +107,9 @@ contract Logs {
         emit publicKeys(r, s, secret, block.timestamp);
     }
 
-<<<<<<< HEAD
+
     function TransferERC20(
-=======
-    function TransferXRC20(
->>>>>>> origin/master
+
         bytes32 r,
         bytes32 s,
         bytes4 secret,
@@ -120,11 +123,9 @@ contract Logs {
 
         publishEphkeys(r, s, secret);
 
-<<<<<<< HEAD
+
         IERC20(token).transferFrom(msg.sender, target, amount);
-=======
-        IXRC20(token).transferFrom(msg.sender, target, amount);
->>>>>>> origin/master
+
         // Perform calculations and updates using temporary variables
         uint256 updatedTotalFunds = totalFunds + amount;
         uint256 updatedLimit = limit + 1;
@@ -135,11 +136,8 @@ contract Logs {
         emit publicKeys(r, s, secret, block.timestamp);
     }
 
-<<<<<<< HEAD
     function TransferERC721(
-=======
-    function TransferXRC721(
->>>>>>> origin/master
+
         bytes32 r,
         bytes32 s,
         bytes4 secret,
@@ -152,11 +150,9 @@ contract Logs {
 
         publishEphkeys(r, s, secret);
 
-<<<<<<< HEAD
+
         IERC721(NftToken).transferFrom(msg.sender, target, tokenId);
-=======
-        IXRC721(NftToken).transferFrom(msg.sender, target, tokenId);
->>>>>>> origin/master
+
         // Perform calculations and updates using temporary variables
         uint256 updatedTotalFunds = totalFunds + tokenId;
         uint256 updatedLimit = limit + 1;
